99cefd3ba426bce3038a6b15d38fa1132cbe5e0f
Gregory Bowne <gbowne144@gmail.com>
2022-08-04 12:23:53 -0700

    Add files via upload
    Added back the files from local.

---
+++ antennacalc.py
@@ -0,0 +1,14 @@
+# inputs for Antenna Calc (mainly for kHz through MHz)
+# chosen frequency && frequency format  Hz, kHz or MHz or GHz.
+# wire size
+# 1/4 wave vertical antenna: 234 / frequency in MHz. Quater-wavelength in meters: 71.5 / frequency in MHz.
+# 3 element Yagi:
+# dipole:
+# 5/8 wave: 585 / frequency in MHz
+# 1/2 wave: 468 or 492 / frequency in MHz 
+# 300, 50, 75 ohm
+# Full Wave: 1005 / frequency in MHz
+# antenna type: vertical, dipole, j-pole, yagi, etc. 
+# yagi # of elements and spacing
+
+# Prints: 
---
+++ sheetmetalcalc.py
@@ -0,0 +1,15 @@
+# This is a sheet metal usage calculator
+# to determine how many parts of a certain size (l x w) be made from
+# a certain size (l x w) sheet of metallic material.
+# allow for 90deg arrangement of parts
+# //
+# input: length of sheet in inches
+# input: width of sheet in inches
+# input: length of part in inches
+# input: width of part in inches
+# input: margin {space in inches between part(s)}
+# input: margin {space in inches to first part and also end part in row and/or column
+#
+# calculations: x area of sheet in sqin
+# //
+# print "x number of parts fit on this sheet in this configuration"
