80d82e1ec576b52d966e181b44227d64b4a435d6
Greg Bowne <gbowne144@gmail.com>
2023-03-17 16:35:55 -0700

    This commit adds the initial work on the program antenna calculator

--- antennacalc.py
+++ antennacalc.py
@@ -1,14 +1,10 @@
-# inputs for Antenna Calc (mainly for kHz through MHz)
-# chosen frequency && frequency format  Hz, kHz or MHz or GHz.
-# wire size
-# 1/4 wave vertical antenna: 234 / frequency in MHz. Quater-wavelength in meters: 71.5 / frequency in MHz.
-# 3 element Yagi:
-# dipole:
-# 5/8 wave: 585 / frequency in MHz
-# 1/2 wave: 468 or 492 / frequency in MHz 
-# 300, 50, 75 ohm
-# Full Wave: 1005 / frequency in MHz
-# antenna type: vertical, dipole, j-pole, yagi, etc. 
-# yagi # of elements and spacing
+import math
 
-# Prints: 
+def dipole():
+    frequency = float(input("Enter your frequency in MHZ to calculate a half-wave dipole."'\n''\n'))
+    print("You entered %s MHZ."'\n' % frequency)
+    length = 468 / frequency
+    element = length / 2
+    print(float(length, 2), "is the total length of the antenna."'\n')
+    print(float(element) 1), "feet, is the length of each element.")
+dipole()
